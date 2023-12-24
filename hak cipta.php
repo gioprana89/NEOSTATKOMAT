9a22d4b82260cedd31e36bedbee5d1f8eaa015f7
gioprana89 <gioprana89@gmail.com>
2023-12-24 15:11:57 +0700

    first commit

--- hak cipta.php
+++ hak cipta.php
@@ -2,19 +2,7 @@
 
 
 <head>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151378191-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-151378191-1');
-</script>
-
-<title>Hak Cipta</title>
-
-<link rel="stylesheet" href="file_css.css">
 
 </head>
 
